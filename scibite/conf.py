d1999b131730efe27f0cb6071860b6260e7edc91
TiagoScibite <tiago@scibite.com>
2021-12-07 13:40:21 +0000

    upgrade to 0.4.6

--- scibite/conf.py
+++ scibite/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, SciBite Data Science'
 author = 'SciBite Data Science Team'
 
 # The full version, including alpha/beta/rc tags
-release = '0.4.5'
+release = '0.4.6'
 
 
 # -- General configuration ---------------------------------------------------
